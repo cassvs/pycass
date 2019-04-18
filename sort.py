df24b81d0ef026ce5e7602743c26730c81070bb3
Cass S <cassvs@users.noreply.github.com>
2019-04-18 14:14:20 -0400

    Added isSorted and bubble sort functions

---
+++ sort.py
@@ -0,0 +1,21 @@
+# sort.py
+# Sorting algorithms
+
+# Checkes if a list is sorted
+def isSorted(list):
+    for i in range(1, len(list)):
+        if(list[i - 1] > list[i]):
+            return False
+    return True
+# End of isSorted function
+
+# Bubble sort
+def bubble(list):
+    while not isSorted(list):
+        for j in range(len(list) - 1):
+            if list[j] > list[j + 1]:
+                swap = list[j + 1]
+                list[j + 1] = list[j]
+                list[j] = swap
+    return list
+# End of bubble function
